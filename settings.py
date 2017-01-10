a613d5727851aee3fdcaba5316e0deebb9db1af1
Pete <peefer1966@gmail.com>
2017-01-10 23:13:25 +0000

    Commit 5-50

--- settings.py
+++ settings.py
@@ -1,16 +1,16 @@
 import os
 
-SECRET_KEY = "youwillneverguess"
-DEBUG = True
-
-# database set-up
+SECRET_KEY = 'you-will-never-guess'
+DEBUG=True
 DB_USERNAME = 'peefer66'
-DB_PASSWORD = ''
+DB_PASSWORD = '' # not required for cloud9
 BLOG_DATABASE_NAME = 'blog'
 DB_HOST = os.getenv('IP', '0.0.0.0')
-
-DB_URI = "mysql+pymysql://{}:{}{}{}".format(DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
-
+DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
+
+
+
+
 
 
