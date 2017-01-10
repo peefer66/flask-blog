13ff5b02e244657e63ce164f0b7bf849e126cb73
Pete <peefer1966@gmail.com>
2017-01-10 09:29:10 +0000

    Commit 09-01-17

--- settings.py
+++ settings.py
@@ -1,6 +1,16 @@
+import os
+
 SECRET_KEY = "youwillneverguess"
-
-
-
-
 DEBUG = True
+
+# database set-up
+DB_USERNAME = 'peefer66'
+DB_PASSWORD = ''
+BLOG_DATABASE_NAME = 'blog'
+DB_HOST = os.getenv('IP', '0.0.0.0')
+
+DB_URI = "mysql+pymysql://{}:{}{}{}".format(DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+
